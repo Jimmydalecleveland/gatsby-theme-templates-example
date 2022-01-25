92716bd6543f13948cdb6bbf7d60a4fc87a167bc
Jimmy DC <jimmydalecleveland@gmail.com>
2022-01-25 10:10:36 -0700

    refactor anonymous function to named

--- example/src/gatsby-theme-minimal/components/Heading.js
+++ example/src/gatsby-theme-minimal/components/Heading.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import Heading from 'gatsby-theme-minimal/src/components/Heading'
 
-export default ({ children }) => <Heading>Shadowed in example site: {children}</Heading>
+const ShadowedHeading = ({ children }) => <Heading>Shadowed in example site: {children}</Heading>
+
+export default ShadowedHeading
--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-// import Heading from '../gatsby-theme-minimal/components/Heading'
 import Heading from 'gatsby-theme-minimal/src/components/Heading'
 
-export default () => <Heading>Homepage in an example site</Heading>
+const HomePage = () => <Heading>Homepage in an example site</Heading>
+
+export default HomePage
